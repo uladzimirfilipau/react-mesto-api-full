12c8308642824407e7f186877d8e327ee45f0204
Vova <uladzimirfilipau@gmail.com>
2022-08-12 19:29:28 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const { validateCreateUser, validateLogin } = require('./middlewares/validate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors);
